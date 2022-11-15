9dbc8bc7a1363cce776e67078511e4ee74aacf1c
edrees abu khames <edreesabukha@gmail.com>
2022-11-15 09:32:42 +0200

    Signup

--- app/src/main/java/com/example/myapplication/SignUp.java
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import android.text.Editable;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,15 +17,17 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link LoginFragment#newInstance} factory method to
+ * Use the {@link SignUp#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class LoginFragment extends Fragment {
-
+public class SignUp extends Fragment {
     private FirebaseServices fbs;
-    private EditText user,password;
+    private EditText pass1,pass2,user;
     private Button btn;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -36,7 +38,7 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public LoginFragment() {
+    public SignUp() {
         // Required empty public constructor
     }
 
@@ -46,11 +48,11 @@ public class LoginFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment LoginFragment.
+     * @return A new instance of fragment SignUp.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
-        LoginFragment fragment = new LoginFragment();
+    public static SignUp newInstance(String param1, String param2) {
+        SignUp fragment = new SignUp();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -71,24 +73,57 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
     }
-
+    public static boolean isEmailValid(String email) {
+        String expression = "^[\\w\\.-]+@([\\w\\-]+\\.)+[A-Z]{2,4}$";
+        Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
     @Override
     public void onStart() {
         super.onStart();
-        connect();
+        Connectcom();
     }
 
-    private void connect() {
-        user=getView().findViewById(R.id.EmailLogin);
-        password=getView().findViewById(R.id.PasswordLogin);
-        btn=getView().findViewById(R.id.LoginMain);
+    public static boolean isValidPassword(String s) {
+        Pattern PASSWORD_PATTERN
+                = Pattern.compile(
+                "[a-zA-Z0-9\\!\\@\\#\\$]{8,24}");
+
+        return !TextUtils.isEmpty(s) && PASSWORD_PATTERN.matcher(s).matches();
+    }
+    private void Connectcom() {
+        btn=getView().findViewById(R.id.btnsign);
+        pass1=getView().findViewById(R.id.zeft);
+        pass2=getView().findViewById(R.id.confirmzeft);
+        user=getView().findViewById(R.id.EtEmail);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String pass=password.getText().toString();
                 String usr=user.getText().toString();
+                String pass=pass1.getText().toString();
+                String confirmpass=pass2.getText().toString();
+                if(usr.isEmpty()||pass.isEmpty()||confirmpass.isEmpty()){
+                    Toast.makeText(getActivity(), "There is something missing", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if(!isEmailValid(pass))
+                {
+                    Toast.makeText(getActivity(), "The Email is not valid!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if(!isValidPassword(pass)) {
+                    Toast.makeText(getActivity(), "The passwords is not valid!!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                // check with confirm
+                if(!confirmpass.equals(pass)){
+                    Toast.makeText(getActivity(), "The passwords not matching!!", Toast.LENGTH_SHORT).show();
+                }
+                // login
                 fbs.getAuth().signInWithEmailAndPassword(usr, pass)
                         .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
                             @Override
@@ -101,7 +136,9 @@ public class LoginFragment extends Fragment {
                                 }
                             }
                         });
+
             }
+
         });
     }
 }
